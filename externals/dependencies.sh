3b3aac9a3390d24852554eb039a726b0c4c5fc0c
SJarod <91637364+SJarod@users.noreply.github.com>
2025-07-21 19:17:37 +0200

    sh dependencies details

--- externals/dependencies.sh
+++ externals/dependencies.sh
@@ -1,3 +1,12 @@
 # Dependency Management Utils
-# bash
+# This manager is based on the bash scripting language
+# ideed it is the most agnosting language
+# to be used with pair with a futur bash build system
+
 # TODO
+
+# features:
+# - clone from urls
+# - analyze path
+# - make installed dependencies and SDK available
+# - translate between many build systems (mainly CMake)
